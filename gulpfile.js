c090c7ebf0dcb8901f748146eb86c49d551362c9
onursabanoglu <>
2022-03-19 12:14:23 +0300

    Add gulpjs file

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+gulp.task('sass', async function () {
+  gulp.src('src/scss/main.scss').pipe(sass()).pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./src/scss/*.scss', gulp.series('sass'));
+});
